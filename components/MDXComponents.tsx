c84f7bec5c85635faa88a9021dbf398bcb025765
mortezahz-cloud <mortezaheydari8742@gmail.com>
2026-04-23 14:31:40 +0330

    Update MDXComponents.tsx

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -4,13 +4,15 @@ import BlogNewsletterForm from 'pliny/ui/BlogNewsletterForm'
 import type { MDXComponents } from 'mdx/types'
 import Image from './Image'
 import CustomLink from './Link'
-import TableWrapper from './TableWrapper'
+import TableOfContents from './TableOfContents'
+import SummaryBox from './SummaryBox'
 
 export const components: MDXComponents = {
   Image,
   TOCInline,
   a: CustomLink,
   pre: Pre,
-  table: TableWrapper,
+  TableOfContents,
   BlogNewsletterForm,
+  SummaryBox, // ثبت باکس خلاصه اختصاصی شما
 }
